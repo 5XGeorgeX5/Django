ed2218d56447970cc8694afa11116734d8272150
George Raafat <georgexraafat@gmail.com>
2024-04-30 10:57:01 +0300

    some work

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'posts/post_list.html', {'posts': posts})
+
+def post_page(request, slug):
+    return HttpResponse(slug)
+    
